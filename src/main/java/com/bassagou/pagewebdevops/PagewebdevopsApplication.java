34c4d77fd84c169536bbdbad065b53039b7040c5
unknown <dikagma@gmail.com>
2026-03-01 11:35:46 +0100

    tp4 v1

---
+++ src/main/java/com/bassagou/pagewebdevops/PagewebdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.bassagou.pagewebdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagewebdevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PagewebdevopsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bassagou/pagewebdevops/web/ProductController.java
@@ -0,0 +1,13 @@
+package com.bassagou.pagewebdevops.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+   @GetMapping("/index")
+    public String index() {
+        return "product";
+    }
+
+}
